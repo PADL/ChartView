419d8021ddfc269f26d3bc73508e301b274845f3
Luke Howard <lukeh@padl.com>
2020-10-11 23:51:16 +1100

    build dynamically

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftUICharts",
+            type: .dynamic,
             targets: ["SwiftUICharts"]),
     ],
     dependencies: [
